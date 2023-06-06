7372555535f69caae32d407861d866cb2a93e786
beingkk <kandersk@gmail.com>
2023-06-06 15:34:13 +0100

    setup files

---
+++ setup.py
@@ -0,0 +1,22 @@
+"""innovation_sweet_spots."""
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read_lines(path):
+    """Read lines of `path`."""
+    with open(path) as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="innovation_sweet_spots",
+    long_description=open("README.md").read(),
+    install_requires=read_lines("requirements.txt"),
+    extras_require={"dev": read_lines("requirements_dev.txt")},
+    packages=find_packages(exclude=["docs"]),
+    version="0.1.0",
+    description="Code associated with Discovery Hub generative AI project",
+    author="Karlis Kanders",
+    license="MIT",
+)

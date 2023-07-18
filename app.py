680e4da227a166f1b9e227a1fda35f555fc9b422
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2023-07-18 19:36:35 +0100

    feat: Add prompt selection

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 """Streamlit app for the Generative AI prototypes."""
 
+from os import listdir
+from os.path import isfile
+from os.path import join
+
 import streamlit as st
 
 from streamlit_option_menu import option_menu
@@ -11,12 +15,12 @@ APP_TITLE = "Nesta Discovery: Generative AI Prototypes"
 
 
 def main() -> None:
-    """Run app."""
+    """Run the app."""
     with st.sidebar:
         selected = option_menu("Prototypes", ["Home page", "ELI3", "EYFS-based activity plan"], default_index=0)
     if selected == "Home page":
         st.title(APP_TITLE)
-        st.write("Welcome to the Nesta Discovery Generative AI prototypes.")
+        st.write("Welcome to the Nesta Discovery Generative AI prototypes. Please select a prototype from the menu.")
     elif selected == "ELI3":
         eli3()
     elif selected == "EYFS-based activity plan":
@@ -93,28 +97,32 @@ def early_year_activity_plan() -> None:
 
     # Create the generator
     selected_model = st.radio(label="**OpenAI model**", options=["gpt-3.5-turbo", "gpt-4"])
-    try:
-        generator = TextGenerator(
-            path="src/genai/ey_activity_plan/prompts/early_year_activity_plan.json",
-            model_name=selected_model,
-        )
-    except Exception:  # Dirty hack to work with local secrets and not break the app on Streamlit Share
-        generator = TextGenerator(
-            api_key=st.secret("OPENAI_API_KEY"),
-            path="src/genai/ey_activity_plan/prompts/early_year_activity_plan.json",
-            model_name=selected_model,
-        )
 
-    prompt_selector = st.radio(label="**Generate with custom prompt**", options=["Default", "Custom"])
+    list_of_prompts = get_prompts(prompts_dir="src/genai/ey_activity_plan/prompts/")
+
+    prompt_selector = st.selectbox(
+        label="**Pick a prompt or write a custom one**", options=list_of_prompts + ["Custom"]
+    )
 
     if prompt_selector == "Custom":
+        generator = load_llm(
+            path_to_prompt="src/genai/ey_activity_plan/prompts/early_year_activity_plan.json",
+            selected_model=selected_model,
+        )
         prompt = st.text_area("Write your own prompt", value=generator.prompt_template.template)
     else:
+        generator = load_llm(
+            path_to_prompt=prompt_selector,
+            selected_model=selected_model,
+        )
         prompt = None
+        with st.expander("**Inspect the prompt**"):
+            # newlines are messed up https://github.com/streamlit/streamlit/issues/868
+            st.write(generator.prompt_template.template)
 
     # Get the user input
     question = st.text_input(
-        label="**What's going on?**",
+        label="**How can I help you?**",
         value="A kid came into my lesson talking about having found a snail in their garden at the weekend.",
         help="Prompt the LLM with a some text and it will generate an activity plan for you.",
     )
@@ -123,6 +131,28 @@ def early_year_activity_plan() -> None:
     if st.button(label="**Generate**", help="Generate an answer."):
         answer = generator.generate(question, prompt=prompt)
         st.write(answer)
+
+
+def get_prompts(prompts_dir: str) -> list:
+    """Get all the prompts from a directory."""
+    return [join(prompts_dir, f) for f in listdir(prompts_dir) if isfile(join(prompts_dir, f))]
+
+
+def load_llm(path_to_prompt: str, selected_model: str) -> TextGenerator:
+    """Load the LLM."""
+    try:
+        generator = TextGenerator(
+            path=path_to_prompt,
+            model_name=selected_model,
+        )
+    except Exception:  # Dirty hack to work with local secrets and not break the app on Streamlit Share
+        generator = TextGenerator(
+            api_key=st.secret("OPENAI_API_KEY"),
+            path=path_to_prompt,
+            model_name=selected_model,
+        )
+
+    return generator
 
 
 if check_password():

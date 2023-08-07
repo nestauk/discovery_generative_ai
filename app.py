ca5f8787c560497923b71222d5737b905103ec33
Kostas Stathoulopoulos <k.stathoylopoylos@gmail.com>
2023-08-07 09:29:51 +0300

    feat: WIP eyfs with bbc data

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import os
 
 import openai
+import pandas as pd
 import streamlit as st
 
 from dotenv import load_dotenv
@@ -21,15 +22,22 @@ APP_TITLE = "Nesta Discovery: Generative AI Prototypes"
 
 def main() -> None:
     """Run the app."""
+    auth_openai()
     with st.sidebar:
-        selected = option_menu("Prototypes", ["Home page", "ELI3", "EYFS-based activity plan"], default_index=0)
+        selected = option_menu(
+            "Prototypes",
+            ["Home page", "ELI3", "EYFS-based activities", "EYFS-based activities with KB"],
+            default_index=0,
+        )
     if selected == "Home page":
         st.title(APP_TITLE)
         st.write("Welcome to the Nesta Discovery Generative AI prototypes. Please select a prototype from the menu.")
     elif selected == "ELI3":
         eli3()
-    elif selected == "EYFS-based activity plan":
+    elif selected == "EYFS-based activities":
         early_year_activity_plan()
+    elif selected == "EYFS-based activities with KB":
+        eyfs_kb()
 
 
 def check_password() -> bool:
@@ -160,6 +168,70 @@ def early_year_activity_plan() -> None:
             )
 
             st.write(r["choices"][0]["message"]["content"])
+
+
+def eyfs_kb() -> None:
+    from genai.eyfs.vector_db import get_embedding
+
+    st.title("Testing")
+
+    collection = read_bbc_and_embed("data/eyfs/tiny_happy_people - final - tiny_happy_people - final.csv")
+    query = st.text_input(
+        label="**What's the topic you want activities for?**",
+        value="sing a lullaby",
+    )
+
+    if st.button(label="**Generate**", help="Generate an answer."):
+        encoded_query = get_embedding(query)
+        r = collection.query(encoded_query, n_results=5)
+
+        for ids, distances, metadatas, texts in zip(r["ids"], r["distances"], r["metadatas"], r["documents"]):
+            for id, distance, metadata, text in zip(ids, distances, metadatas, texts):
+                st.write(
+                    f"""#### {id}\n\n**Euclidean distance**: {distance}\n\n**Text**: {text}\n\n**Metadata**: {metadata}"""
+                )
+
+        st.write(r)
+
+
+@st.cache_resource
+def read_bbc_and_embed(
+    path: str = "data/eyfs/tiny_happy_people - final - tiny_happy_people - final.csv",
+) -> pd.DataFrame:
+    """Read the BBC dataset."""
+    import chromadb
+
+    from genai.eyfs.vector_db import batch
+    from genai.eyfs.vector_db import get_embedding
+
+    df = pd.read_csv(path)
+    df = df.rename(columns={"Age Range (if applicable)": "Age"})
+    df = df.dropna(subset=["text", "URL"])
+    df = df.drop_duplicates(subset=["URL"])
+    df["embedding"] = df["text"].apply(lambda row: get_embedding(row, model="text-embedding-ada-002"))
+
+    embeddings = batch(df["embedding"].tolist(), 200)
+    url = batch(df["URL"].tolist(), 200)
+    age = batch(df["Age"].tolist(), 200)
+    activity_type = batch(df["Type"].tolist(), 200)
+    text = batch(df["text"].tolist(), 200)
+
+    # Build a quick and dirty index
+    client = chromadb.Client()
+
+    collection = client.create_collection("eyfs_kb3")
+
+    for batch_embeddings, batch_url, batch_age, batch_activity_type, batch_text in zip(
+        embeddings, url, age, activity_type, text
+    ):
+        collection.add(
+            ids=batch_url,
+            embeddings=batch_embeddings,
+            metadatas=[{"age": age, "activity": activity} for age, activity in zip(batch_age, batch_activity_type)],
+            documents=batch_text,
+        )
+
+    return collection
 
 
 def auth_openai() -> None:
---
+++ src/genai/eyfs/vector_db.py
@@ -0,0 +1,15 @@
+import chromadb
+import openai
+
+from dotenv import load_dotenv
+
+
+def get_embedding(text, model="text-embedding-ada-002"):
+    text = text.replace("\n", " ")
+    return openai.Embedding.create(input=[text], model=model)["data"][0]["embedding"]
+
+
+def batch(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i : i + n]  # noqa: E203
